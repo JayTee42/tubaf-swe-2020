cd51748d45d56808dcf79d5023a8d12236f16f74
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2020-04-27 19:44:03 +0200

    Fixed indentation

--- 01-Eingabe, Ausgabe und Datentypen/Temperature.cs
+++ 01-Eingabe, Ausgabe und Datentypen/Temperature.cs
@@ -2,18 +2,18 @@ using System;
 
 namespace TemperatureCalculator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            float fahrenheit, celsius;
-            celsius = 0;
-            fahrenheit = (1.8f * celsius) + 32;
-            Console.WriteLine("{0} Celsius entspricht {1} Fahrenheit", celsius, fahrenheit);
-            Console.WriteLine("Celsius: ");
-            celsius = Convert.ToSingle(Console.ReadLine()); //Exception handling!
-            fahrenheit = (1.8f * celsius) + 32;
-            Console.WriteLine("Fahrenheit: {0}", fahrenheit);
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float fahrenheit, celsius;
+			celsius = 0;
+			fahrenheit = (1.8f * celsius) + 32;
+			Console.WriteLine("{0} Celsius entspricht {1} Fahrenheit", celsius, fahrenheit);
+			Console.WriteLine("Celsius: ");
+			celsius = Convert.ToSingle(Console.ReadLine()); //Exception handling!
+			fahrenheit = (1.8f * celsius) + 32;
+			Console.WriteLine("Fahrenheit: {0}", fahrenheit);
+		}
+	}
 }
