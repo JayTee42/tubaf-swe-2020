307d50ec510b5e8c041142d6c48c9a8d26066bb4
JayTee <endoplasmaticreticulum@yahoo.de>
2020-05-04 19:28:08 +0200

    Update Arithmetic.cs

--- 02-Arithmetische und bitweise Operationen/Arithmetic.cs
+++ 02-Arithmetische und bitweise Operationen/Arithmetic.cs
@@ -10,16 +10,16 @@ namespace Artihmetics
 			double y = 4.0;
 
 			var d = (double)a / b;
-			Console.WriteLine($"d = { d }");
+			Console.WriteLine($"d = {d}");
 
 			var f = ((a + b) / (c - y)) - d;
-			Console.WriteLine($"f = { f }");
+			Console.WriteLine($"f = {f}");
 
 			var x0 = (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
-			Console.WriteLine($"x0 = { x0 }");
+			Console.WriteLine($"x0 = {x0}");
 
 			var area = Math.PI * r * r;
-			Console.WriteLine($"area = { area }");
+			Console.WriteLine($"area = {area}");
 		}
 	}
 }
